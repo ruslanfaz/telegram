d806f728d22ad02b7e9c568cb6738999d4b92dab
zhuravsky <ruselfaz@mail.ru>
2024-12-25 17:49:48 +0500

    Go!

--- ruselfaz_bot.py
+++ ruselfaz_bot.py
@@ -1,1 +1,11 @@
-`111`fd
+import telebot
+
+token = "7881582171:AAFLSKL5dl_UGzy2X6xKs7X1IcoUDQ2xn30"
+
+bot = telebot.TeleBot(token)
+
+@bot.message_handler(content_types=["text"])
+def start(message):
+    bot.send_message(message.chat.id, message.text)
+
+bot.infinity_polling()
